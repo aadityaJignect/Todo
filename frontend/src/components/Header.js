6e7e471d0bce37ebf9e70f56d9918149ce1bca19
Aaditya Sardar <aaditya@jignect.local>
2025-12-19 17:53:47 +0530

    this is temp change to check the git

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ toggleTheme, isDark }) => {
   return (
     <header className="header">
-      <h1>Todo App</h1>
+      <h1>Todo App updated at 17:52 pm</h1>
       <button className="toggle-btn" onClick={toggleTheme}>
         {isDark ? '☀️ Light' : '🌙 Dark'} Mode
       </button>
